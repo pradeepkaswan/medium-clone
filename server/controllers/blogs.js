a86cb832965f20e2376f1e88563ad01eb57b00a9
Pradeep Kaswan <pradeepkaswan7@gmail.com>
2023-12-12 11:39:40 +0530

    feat: setup app for production

---
+++ server/controllers/blogs.js
@@ -0,0 +1,51 @@
+import Blog from '../models/Blog.js';
+
+export const getBlogs = async (req, res) => {
+	try {
+		const blogs = await Blog.find();
+		res.status(200).json(blogs);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+export const getBlogById = async (req, res) => {
+	try {
+		const blog = await Blog.findById(req.params.id);
+		res.status(200).json(blog);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+export const createBlog = async (req, res) => {
+	const { title, content } = req.body;
+
+	try {
+		const newBlog = new Blog({ title, content });
+		const savedBlog = await newBlog.save();
+		res.status(201).json(savedBlog);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+export const updateBlog = async (req, res) => {
+	try {
+		const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+		});
+		res.status(200).json(updatedBlog);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+export const deleteBlog = async (req, res) => {
+	try {
+		const deletedBlog = await Blog.findByIdAndRemove(req.params.id);
+		res.status(200).json(deletedBlog);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
--- server/server.js
+++ server/server.js
@@ -1,31 +1,31 @@
-import express from 'express';
-import authRoutes from './routes/auth.js';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import admin from 'firebase-admin';
-import serviceAccountKey from './config/service-account-key.json' assert { type: 'json' };
+import express from 'express'
+import authRoutes from './routes/auth.js'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import admin from 'firebase-admin'
+import serviceAccountKey from './config/service-account-key.json' assert { type: 'json' }
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
 // Firebase
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccountKey),
-});
+  credential: admin.credential.cert(serviceAccountKey),
+})
 
 // Middleware
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes)
 
-connectDB();
+connectDB()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
